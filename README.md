0093e14e9b3f5b43b9ab17e8a4dbebb5faf6f9c7
ovalery16 <olivier_valery@hotmail.com>
2018-11-02 17:14:31 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Analyze Motion Capture Hand Postures
+
+Consider the “Motion Capture Hand Postures” dataset, whose description is here https://archive.ics.uci.edu/ml/datasets/Motion+Capture+Hand+Postures, it has been asked to analyze these data via a shallow deep neural network.
+
+This project consists of several documents:
+* This intruction of this coding challenge can be found in the file "coding test". 
+* The coding report is called "report_interview"
+* The network weights *.h5
+* The python code *.py
+
+If you like you like this project, feel free to leave a star. (it is my only reward ^^)
---
+++ train_class_only.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import np_utils
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+# load dataset
+dataframe = pd.read_csv('Postures.csv')
+
+
+for val in list(dataframe.columns.values):
+    dataframe[val] = pd.to_numeric(dataframe[val], errors='coerce')
+
+    
+dataframe=dataframe.fillna(dataframe.mean())
+
+
+listValuesToNormalize=list(dataframe.columns.values)
+listValuesToNormalize.remove('Class')
+listValuesToNormalize.remove('User')
+listValuesToNormalize
+dataframe[listValuesToNormalize] = minmax_scale(dataframe[listValuesToNormalize])
+
+# get dataset (split it in input/output)
+dataset = dataframe.values
+
+X = dataset[:,2:].astype(float)
+Y = dataset[:,0].astype(int)
+
+# convert integers to  one hot encoded
+hot_encoded_y = np_utils.to_categorical(Y)
+
+#split 0.8/0.2
+X_train, X_test, y_train, y_test = train_test_split(X, hot_encoded_y, test_size=0.2, random_state=1)
+
+
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
+
+print("the dataset has "+str(X.shape[0])+ "samples that are splitted in:")
+print("- "+str(X_train.shape[0])+"samples (training set)" )
+print("- "+str(X_val.shape[0])+"samples (validation set)")
+print("- "+str(X_test.shape[0])+"samples (test set)")
+
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=36, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(10, activation='relu'))
+#model.add(Dropout(0.7371698374615214))
+model.add(Dense(6, activation='softmax'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_val,y_val), epochs=160, batch_size=40)
+
+#test model
+loss, acc =  model.evaluate(X_test, y_test, verbose=0)
+print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
+model.save('my_model_only_class_v2.h5')
